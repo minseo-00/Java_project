66cb6ddc3e1d4499580e1b0668fae613b0aaa1e0
minseo <limminsoe08@gmail.com>
2025-05-18 20:45:26 +0900

    장바구니 항목 삭제 및 비우기

--- bookmarket0/CartItem.java
+++ bookmarket0/CartItem.java
@@ -1,7 +1,8 @@
 package bookmarket0;
 
 public class CartItem {
-	private String[] itemBook = new String[7];
+//	private String[] itemBook = new String[7];
+	private Book itemBook;
 	private String bookId;
 	private int quantity;
 	private int totalPrice;
@@ -10,19 +11,32 @@ public class CartItem {
 	
 	}
 	
-	public CartItem(String[] book) {
-		this.itemBook = book;
-		this.bookId = book[0];
+//	public CartItem(String[] book) {
+//		this.itemBook = book;
+//		this.bookId = book[0];
+//		this.quantity = 1;
+//		updateTotalPrice();
+//	}
+//	
+//	public String[] getItemBook() {
+//		return itemBook;
+//	}
+//	
+//	public void setItemBook(String[] itemBook) {
+//		this.itemBook = itemBook;
+//	}
+	
+	public CartItem(Book booklist) {
+		this.itemBook = booklist;
+		this.bookId = booklist.getBookId();
 		this.quantity = 1;
 		updateTotalPrice();
 	}
-	
-	public String[] getItemBook() {
-		return itemBook;
-	}
-	
-	public void setItemBook(String[] itemBook) {
+	public void setItemBook(Book itemBook) {
 		this.itemBook = itemBook;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
 	}
 	
 	public String getBookID() {
@@ -47,7 +61,8 @@ public class CartItem {
 	}
 	
 	public void updateTotalPrice() {
-		totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
+//		totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
+		totalPrice = this.itemBook.getUnitPrice() * this.quantity;
 	}
 
 }
